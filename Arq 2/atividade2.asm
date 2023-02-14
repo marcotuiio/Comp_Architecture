c173b2b999ecd56af03d9a6e5d75482007dd4a32
marcotulio <marcotulio.barros@uel.br>
2023-02-14 15:12:10 -0300

    Updated ativ2

--- Arq 2/atividade2.asm
+++ Arq 2/atividade2.asm
@@ -18,15 +18,18 @@
       	li $s1, 9999
       	li $s3, -9999
       	li $s6, 2
-      	jal menor_maior
+      	jal menor
+      	
+      	move $a0, $v0
+      	jal maior_impar
       	
       	print_str("\n Menor elemento é ")
       	print_int($s1)
-      	print_str(" e está na linha: ")
+      	print_str(" e está na linha ")
       	print_int($s2)
       	print_str("\n Maior elemento ímpar é ")
       	print_int($s3)
-      	print_str(" e está na linha: ")
+      	print_str(" e está na linha ")
       	print_int($s4)
 		
 		terminate
@@ -90,23 +93,16 @@
    	## a elemento e armazenando os valores de interesse
    	## $s1 armazena o menor elemento e $s2 armazena a linha do mesmo
    	## $s3 armazena o maior elemento ímpar e $s4 armazena a linha do mesmo	
-   	menor_maior:
+   	menor:
    		subi $sp, $sp, 4 # Espaço para 1 item na pilha
    		sw $ra, ($sp) # Salva o retorno para a main
    		move $a3, $a0 # aux = endereço base de mat
    	min:
    		jal indice	
    		lw $a0, ($v0) # valor em mat[i][j]
-   		bgt $a0, $s1, max
+   		bgt $a0, $s1, next
    		move $s1, $a0
-   		move $s2, $t0
-   		max:
-   			blt $a0, $s3, next
-   			move $s3,  $a0
-   			div $s3, $s6  # divisão do valor atual por 2
-   			mfhi $s7  # resto da div em $s7
-   			beq $s7, 0, next
-   			move $s4, $t0	
+   		move $s2, $t0	
    		next:
    			addi $t1, $t1, 1 # j++
    			blt $t1, $a2, min # if(j < ncol) goto e
@@ -118,4 +114,27 @@
    			addi $sp, $sp, 4 # Libera o espaço na pilha
   			move $v0, $a3 # Endereço base da matriz para retorno
    			jr $ra # Retorna para a main
+   		
+	maior_impar:
+		subi $sp, $sp, 4 # Espaço para 1 item na pilha
+   		sw $ra, ($sp) # Salva o retorno para a main
+   		move $a3, $a0 # aux = endereço base de mat
+	max:
+		jal indice # Calcula o endereço de mat[i][j]
+		lw $a0, ($v0) # valor em mat[i][j]
+   		blt $a0, $s3, next2
+   		move $s3, $a0
+   		div $s3, $s6  # divisão do valor atual por 2
+   		mfhi $s7  # resto da div em $s7
+   		beq $s7, 0, next2
+   		move $s4, $t0
+   		next2:
+   			li $t1, 0 # j = 0
+   			addi $t0, $t0, 1 # i++
+   			blt $t0, $a1, max # if(i < nlin) goto e
+	   		li $t0, 0 # i = 0
+		   	lw $ra, ($sp) # Recupera o retorno para a main
+   			addi $sp, $sp, 4 # Libera o espaço na pilha
+  			move $v0, $a3 # Endereço base da matriz para retorno
+ 	  		jr $ra # Retorna para a main
    		
