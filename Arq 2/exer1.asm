c7f70492de43765064c3a8cfc1600a4f994024ae
marcotulio <marcotulio.barros@uel.br>
2023-02-15 13:44:23 -0300

    New matrix exercises complete

--- Arq 2/exer1.asm
+++ Arq 2/exer1.asm
@@ -14,24 +14,53 @@
       	move $a0, $v0 # Endereço da matriz lida
       	jal escrita # escrita(mat, nlin, ncol)
       	
-      	move $a0, $v0
-      	li $s1, 9999
-      	li $s3, -9999
-      	li $s6, 2
-      	jal menor
+      	li $s0, 9999 # Ao final, o menor valor abaixo da diagonal principal estará em $s0
+      	li $s1, -9999 # Ao final, o maior valor acima da diagonal principal estará em $s1
+      	li $s2, 0 # A = Soma dos valores acima da diag prin
+      	li $s3, 0 # B = Soma dos valores abaixo da diag prin
+      	li $s4, 0 # Subtração A-B
       	
       	move $a0, $v0
-      	jal maior_impar
+      	jal menor_maior
       	
-      	print_str("\n Menor elemento é ")
+      	sub $s4, $s2, $s3
+      	
+      	print_str("\n Menor elemento abaixo da diagonal principal é ")
+      	print_int($s0)
+      	print_str("\n Maior elemento acima da diagonal principal é ")
       	print_int($s1)
-      	print_str(" e está na linha ")
+      	print_str("\n Subtração A - B = ")
       	print_int($s2)
-      	print_str("\n Maior elemento ímpar é ")
+      	print_str(" - ")
       	print_int($s3)
-      	print_str(" e está na linha ")
+      	print_str(" = ")
       	print_int($s4)
-		
+      	
+      	la $a0, Mat
+      	move $a0, $v0
+      	mul $s0, $a1, $a1
+      	
+      	print_str("\n\n Matriz ordenada em ordem crescente: \n")
+      	sort_array(Mat, $s0)
+      	
+		li $t1, 0
+		la $t2, Mat
+		li $t7, 0
+		loop_print:	
+				beq $s0, $t1, exit  # Se tamanho mÃ¡ximo do vetor ja foi alcançado, encerrar
+				sll $t3, $t1, 2  # Reg temp $t1 = 4*i (indice atual do vetor)
+				add $t3, $t3, $t2  # Carregando em $t1 = endereço de vetor[i] 
+				lw $t4, 0($t3)  # $t2 = valor de vetor[i]
+				print_int($t4)
+				print_str(" ")
+				addi $t1, $t1, 1  # Atualizando i = i+1
+				addi $t7, $t7, 1
+				bne $t7, $a1, loop_print
+				print_str("\n")
+				li $t7, 0
+				j loop_print
+		exit:
+      	
 		terminate
 		
 	indice:
@@ -89,52 +118,36 @@
   		move $v0, $a3 # Endereço base da matriz para retorno
    		jr $ra # Retorna para a main
    		
-   	## Função que utiliza a função de indice para percorrer a matriz e assim vai comparando elemento
-   	## a elemento e armazenando os valores de interesse
-   	## $s1 armazena o menor elemento e $s2 armazena a linha do mesmo
-   	## $s3 armazena o maior elemento ímpar e $s4 armazena a linha do mesmo	
-   	menor:
+   	## Os elementos abaixo da diag princ têm i > j, ou seja, $t0 > $t1 	
+   	## enquanto os elementos acima da diag princ têm i < j, ou seja, $t0 < $t1
+   	menor_maior:
    		subi $sp, $sp, 4 # Espaço para 1 item na pilha
    		sw $ra, ($sp) # Salva o retorno para a main
    		move $a3, $a0 # aux = endereço base de mat
-   	min:
-   		jal indice	
-   		lw $a0, ($v0) # valor em mat[i][j]
-   		bgt $a0, $s1, next
+	shared:
+		jal indice # Calcula o endereço de mat[i][j]
+   		lw $a0, ($v0) # Valor em mat[i][j]
+   		beq $t0, $t1, next
+   		bgt $t0, $t1, menor # $t0 > $t1 vai pra menor
+   		maior:
+   		add $s2, $s2, $a0
+   		blt $a0, $s1, next
    		move $s1, $a0
-   		move $s2, $t0	
+   		j next
+   		menor: 
+   		add $s3, $s3, $a0 # soma dos abaixo
+   		bgt $a0, $s0, next # se valor em $a0 for maior que o de $s0, não troca
+   		move $s0, $a0 # colocando em $s0 o menor valor
+   		
    		next:
    			addi $t1, $t1, 1 # j++
-   			blt $t1, $a2, min # if(j < ncol) goto e
+   			blt $t1, $a2, shared # if(j < ncol) goto e
    			li $t1, 0 # j = 0
    			addi $t0, $t0, 1 # i++
-   			blt $t0, $a1, min # if(i < nlin) goto e
+   			blt $t0, $a1, shared # if(i < nlin) goto e
    			li $t0, 0 # i = 0
 	   		lw $ra, ($sp) # Recupera o retorno para a main
    			addi $sp, $sp, 4 # Libera o espaço na pilha
   			move $v0, $a3 # Endereço base da matriz para retorno
    			jr $ra # Retorna para a main
-   		
-	maior_impar:
-		subi $sp, $sp, 4 # Espaço para 1 item na pilha
-   		sw $ra, ($sp) # Salva o retorno para a main
-   		move $a3, $a0 # aux = endereço base de mat
-	max:
-		jal indice # Calcula o endereço de mat[i][j]
-		lw $a0, ($v0) # valor em mat[i][j]
-   		blt $a0, $s3, next2
-   		move $s3, $a0
-   		div $s3, $s6  # divisão do valor atual por 2
-   		mfhi $s7  # resto da div em $s7
-   		beq $s7, 0, next2
-   		move $s4, $t0
-   		next2:
-   			li $t1, 0 # j = 0
-   			addi $t0, $t0, 1 # i++
-   			blt $t0, $a1, max # if(i < nlin) goto e
-	   		li $t0, 0 # i = 0
-		   	lw $ra, ($sp) # Recupera o retorno para a main
-   			addi $sp, $sp, 4 # Libera o espaço na pilha
-  			move $v0, $a3 # Endereço base da matriz para retorno
- 	  		jr $ra # Retorna para a main
-   		
+   			
--- Arq 2/exer2.asm
+++ Arq 2/exer2.asm
@@ -1,39 +1,28 @@
 .include "macros.asm"
 
 .data
-	Mat: .space 400 # 10x10 * 4 (inteiro)
+	Mat: .space 64 # 4*4 * 4 (inteiro)
 	
 .text
 	main:
-		print_str("\nInforme a dimensão N da matriz quadrada: ")
-		scan_int($a1) # Número de linhas
-	  
+		li $a1, 4
 		la $a0, Mat # Endereço base de Mat
       	addi $a2, $a1, 0 # Número de colunas
       	jal leitura # leitura(mat, nlin, ncol)
       	move $a0, $v0 # Endereço da matriz lida
       	jal escrita # escrita(mat, nlin, ncol)
-      	
-      	move $a0, $v0
-      	li $s1, 9999
-      	li $s3, -9999
-      	li $s6, 2
-      	jal menor
-      	
-      	move $a0, $v0
-      	jal maior_impar
-      	
-      	print_str("\n Menor elemento é ")
-      	print_int($s1)
-      	print_str(" e está na linha ")
-      	print_int($s2)
-      	print_str("\n Maior elemento ímpar é ")
-      	print_int($s3)
-      	print_str(" e está na linha ")
-      	print_int($s4)
 		
-		terminate
+		li $s0, 0 # Soma dos valores na diagonal secundária
+		li $s1, 0 # Aux para armazenar a soma de i + j
 		
+		move $a0, $v0
+		jal diag_secund
+		
+		print_str("\n A soma dos elementos da diagonal secundária é ")
+      	print_int($s0)
+      	
+      	terminate
+      	
 	indice:
 		mul $v0, $t0, $a2 # i * ncol
    		add $v0, $v0, $t1 # (i * ncol) + j
@@ -89,52 +78,30 @@
   		move $v0, $a3 # Endereço base da matriz para retorno
    		jr $ra # Retorna para a main
    		
-   	## Função que utiliza a função de indice para percorrer a matriz e assim vai comparando elemento
-   	## a elemento e armazenando os valores de interesse
-   	## $s1 armazena o menor elemento e $s2 armazena a linha do mesmo
-   	## $s3 armazena o maior elemento ímpar e $s4 armazena a linha do mesmo	
-   	menor:
-   		subi $sp, $sp, 4 # Espaço para 1 item na pilha
-   		sw $ra, ($sp) # Salva o retorno para a main
-   		move $a3, $a0 # aux = endereço base de mat
-   	min:
-   		jal indice	
-   		lw $a0, ($v0) # valor em mat[i][j]
-   		bgt $a0, $s1, next
-   		move $s1, $a0
-   		move $s2, $t0	
+   		## A diagonal principal da matriz de ordem n tem elementos em que i + j = n + 1
+   		## Neste exemplo a ordem da matriz é de ordem 4, a comparação será feita se i+j do 
+   		## elemento em questão é igual a 5, se for a soma é realizada, se não a matriz continua
+   		## até o fim
+   		diag_secund:
+   			subi $sp, $sp, 4 # Espaço para 1 item na pilha
+   			sw $ra, ($sp) # Salva o retorno para a main
+   			move $a3, $a0 # aux = endereço base de mat
+		ds:
+			jal indice # Calcula o endereço de mat[i][j]
+   			lw $a0, ($v0) # Valor em mat[i][j]
+   			add $t5, $t0, 1 # Posição aux de i
+   			add $t6, $t1, 1 # Posição aux de j
+   			add $s1, $t5, $t6
+   			bne $s1, 5, next
+   			add $s0, $s0, $a0
    		next:
    			addi $t1, $t1, 1 # j++
-   			blt $t1, $a2, min # if(j < ncol) goto e
+   			blt $t1, $a2, ds # if(j < ncol) goto e
    			li $t1, 0 # j = 0
    			addi $t0, $t0, 1 # i++
-   			blt $t0, $a1, min # if(i < nlin) goto e
+   			blt $t0, $a1, ds # if(i < nlin) goto e
    			li $t0, 0 # i = 0
 	   		lw $ra, ($sp) # Recupera o retorno para a main
    			addi $sp, $sp, 4 # Libera o espaço na pilha
   			move $v0, $a3 # Endereço base da matriz para retorno
-   			jr $ra # Retorna para a main
-   		
-	maior_impar:
-		subi $sp, $sp, 4 # Espaço para 1 item na pilha
-   		sw $ra, ($sp) # Salva o retorno para a main
-   		move $a3, $a0 # aux = endereço base de mat
-	max:
-		jal indice # Calcula o endereço de mat[i][j]
-		lw $a0, ($v0) # valor em mat[i][j]
-   		blt $a0, $s3, next2
-   		move $s3, $a0
-   		div $s3, $s6  # divisão do valor atual por 2
-   		mfhi $s7  # resto da div em $s7
-   		beq $s7, 0, next2
-   		move $s4, $t0
-   		next2:
-   			li $t1, 0 # j = 0
-   			addi $t0, $t0, 1 # i++
-   			blt $t0, $a1, max # if(i < nlin) goto e
-	   		li $t0, 0 # i = 0
-		   	lw $ra, ($sp) # Recupera o retorno para a main
-   			addi $sp, $sp, 4 # Libera o espaço na pilha
-  			move $v0, $a3 # Endereço base da matriz para retorno
- 	  		jr $ra # Retorna para a main
-   		
+   			jr $ra # Retorna para a main	
